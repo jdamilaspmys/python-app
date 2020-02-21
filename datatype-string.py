69831aaebb372d8dd9a126210cb6bd602d028a01
jdamilaspmys <jdamilaspmys@gmail.com>
2020-02-21 15:16:47 +0800

    example datatype string more

--- datatype-string.py
+++ datatype-string.py
@@ -6,3 +6,7 @@ print (str[2:5])     # Prints characters starting from 3rd to 5th
 print (str[2:])      # Prints string starting from 3rd character
 print (str * 2 )     # Prints string two times
 print (str + "TEST") # Prints concatenated string
+
+#  More example 
+print (str[-2:])   # Print last 2 characters 
+print (str[:-2])   # Print execpt last 2 characters 
